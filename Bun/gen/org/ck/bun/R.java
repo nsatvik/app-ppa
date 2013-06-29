7ccc5b07f17a51cb258dd6be1cd3373a57495517
nsatvik <nsatvik@gmail.com>
2013-06-29 13:36:29 +0530

    UI Changes
    Very very Basic UI changes

--- Bun/gen/org/ck/bun/R.java
+++ Bun/gen/org/ck/bun/R.java
@@ -21,13 +21,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bun=0x7f020000;
+        public static final int face=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int head_image=0x7f080000;
+        public static final int textViewTitle=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int window_title=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- Bun/src/org/ck/bun/MainActivity.java
+++ Bun/src/org/ck/bun/MainActivity.java
@@ -1,23 +1,32 @@
 package org.ck.bun;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.support.v4.app.Fragment;
+import android.view.Window;
 public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		 setContentView(R.layout.activity_main);
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
+		setClickListeners();
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+	private void setClickListeners() {
+		//To be added!
+		
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
